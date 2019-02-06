fe1dbb1d7dbc2ab7612b6b7aa0994f98b3ef6d6f
Orbergs <kjell@wtmg.se>
2019-02-06 16:31:32 +0100

    First commit

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  template: '<h2>Hello World</h2><img src="/assets/images/basic-shield.png"/>'
+})
+export class EventsAppComponent {
+  title = 'app';
+}
